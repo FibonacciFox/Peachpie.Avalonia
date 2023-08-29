1e855835e1c515e8e035990954a6cca80bbe65d8
FibonacciFox <shamanwolfrus@gmail.com>
2023-08-30 00:15:37 +0700

    Базоввый пример работы с axaml формой.

--- MyPhpLib/src/MyPhpLib/PHPWindow.php
+++ MyPhpLib/src/MyPhpLib/PHPWindow.php
@@ -4,18 +4,30 @@ namespace MyPhpLib {
 
     use Avalonia\Markup\Xaml\AvaloniaXamlLoader;
     use Peachpie\Avalonia\Controls\Buttons\UxButton;
+    use Peachpie\Avalonia\Controls\Layout\UxStackPanel;
+    use Peachpie\Avalonia\Controls\TextDisplay\UxTextBlock;
     use Peachpie\Avalonia\Controls\Windows\UxWindow;
 
     class PHPWindow extends UxWindow
     {
         public UxButton $MyUxButton;
-
+        public UxStackPanel $MyUxStackPanel;
 
         public function __construct()
         {
+            //Сначала всегда инициализируем компонент формы.
             $this->InitializeComponent();
 
             $this->MyUxButton = $this->FindByName("MyUxButton");
+            $this->MyUxStackPanel = $this->FindByName("MyUxStackPanel");
+
+            $this->MyUxButton->on("Click", function (UxButton $button, $args) {
+                $text = new UxTextBlock();
+                $text->HorizontalAlignment = \Avalonia\Layout\HorizontalAlignment::Center;
+                $text->Text = "Added New Text Block";
+                $this->MyUxStackPanel->Children->Add($text);
+            });
+
         }
 
         public function InitializeComponent(): void
