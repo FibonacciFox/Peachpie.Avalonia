8226821c4c35bf771266aa640c64e2419551da09
FibonacciFox <shamanwolfrus@gmail.com>
2024-06-13 23:27:53 +0700

    Fix Timer

--- PeachPie Extension Libraries/Peachpie.Base.Ext/.sdk/Php/Threading/Timer.php
+++ PeachPie Extension Libraries/Peachpie.Base.Ext/.sdk/Php/Threading/Timer.php
@@ -2,80 +2,100 @@
 
 namespace Php\Threading;
 
-
 class Timer
 {
     /**
-     * Timer constructor.
+     * Конструктор таймера / Timer constructor.
      */
     protected function __construct()
     {
     }
 
     /**
-     * Run once after the period in format:
-     *  '' - millis
-     *  's' - seconds
-     *  'm' - minutes
-     *  'h' - hours
-     *  'd' - days (24 hours)
+     * Выполнить один раз после указанного периода в формате:
+     * '' - миллисекунды
+     * 's' - секунды
+     * 'm' - минуты
+     * 'h' - часы
+     * 'd' - дни (24 часа)
      *
-     *  for example '2h 30m 10s' or '2.5s' or '2000' or '1m 30s'
+     * например '2h 30m 10s' или '2.5s' или '2000' или '1m 30s'
+     *
+     * Run once after the period in format:
+     * '' - millis
+     * 's' - seconds
+     * 'm' - minutes
+     * 'h' - hours
+     * 'd' - days (24 hours)
+     *
+     * for example '2h 30m 10s' or '2.5s' or '2000' or '1m 30s'
      *
      * @param string $period
      * @param callable $callback
      * @return Timer
      */
-    static function after(string $period, callable $callback) : Timer
+    static function after(string $period, callable $callback): Timer
     {
     }
 
     /**
+     * Выполнять каждый раз с указанным периодом как в методе after().
+     *
      * Run every time based on the period as in after() method.
      *
      * @param string $period
      * @param callable $callback
      * @return Timer
      */
-    static function every($period, callable $callback) : Timer
+    static function every(string $period, callable $callback): Timer
     {
     }
 
     /**
-     * Create trigger timer which once call when trigger callback will return true.
+     * Создать триггерный таймер, который сработает, когда триггерный callback вернет true.
+     *
+     * Create trigger timer which will call when trigger callback returns true.
      *
      * @param callable $trigger (): bool
      * @param callable $callback
-     * @param int $checkPeriod time period of checking trigger (default 1/60 sec).
+     * @param int $checkPeriod Time period of checking trigger (default 1/60 sec).
      * @return Timer
      */
-    static function trigger(callable $trigger, callable $callback, int $checkPeriod = 16) : Timer
+    static function trigger(callable $trigger, callable $callback, int $checkPeriod = 16): Timer
     {
     }
 
     /**
+     * Как в JS.
+     *
      * Like in JS.
      *
      * @param callable $taskCallback
-     * @param int $millis delay period in millis.
+     * @param int $millis Delay period in millis.
      * @return Timer
      */
-    static function setTimeout(callable $taskCallback, $millis)
+    static function setTimeout(callable $taskCallback, int $millis): Timer
     {
     }
 
     /**
+     * Как в JS.
+     *
      * Like in JS.
+     *
      * @param callable $taskCallback
      * @param int $millis
      * @return Timer
      */
-    static function setInterval(callable $taskCallback, $millis) : Timer
+    static function setInterval(callable $taskCallback, int $millis): Timer
     {
     }
 
     /**
+     * Задержка выполнения на указанный период.
+     *
      * Sleep period time.
+     *
      * @param string $period
      */
     static function sleep(string $period)
@@ -83,6 +103,8 @@ class Timer
     }
 
     /**
+     * Отменить все запланированные задачи таймера.
+     *
      * Call all timer tasks which scheduled.
      */
     static function cancelAll()
@@ -90,6 +112,8 @@ class Timer
     }
 
     /**
+     * Завершить все таймеры и систему таймеров!
+     *
      * Shutdown all timers and timer system!
      */
     static function shutdownAll()
@@ -97,24 +121,36 @@ class Timer
     }
 
     /**
+     * Преобразует строковый период в количество миллисекунд.
+     *
      * Converts a string period to amount of millis.
      *
-     * '' - millis
-     *  's' - seconds
-     *  'm' - minutes
-     *  'h' - hours
-     *  'd' - days (24 hours)
+     * '' - миллисекунды
+     * 's' - секунды
+     * 'm' - минуты
+     * 'h' - часы
+     * 'd' - дни (24 часа)
      *
-     *  for example '2h 30m 10s' or '2.5s' or '2000' or '1m 30s'
+     * например '2h 30m 10s' или '2.5s' или '2000' или '1m 30s'
+     *
+     * '' - millis
+     * 's' - seconds
+     * 'm' - minutes
+     * 'h' - hours
+     * 'd' - days (24 hours)
+     *
+     * for example '2h 30m 10s' or '2.5s' or '2000' or '1m 30s'
      *
      * @param string $value
      * @return int
      */
-    static function parsePeriod(string $value) : int
+    static function parsePeriod(string $value): int
     {
     }
 
     /**
+     * Отменить задачу таймера.
+     *
      * Cancel timer task.
      */
     function cancel()
@@ -122,19 +158,76 @@ class Timer
     }
 
     /**
-     * Run timer task.
+     * Немедленно запустить задачу таймера.
+     *
+     * Run timer task immediately.
      */
     function run()
     {
     }
 
     /**
+     * Остановить задачу таймера.
+     *
+     * Stop timer task.
+     */
+    function stop()
+    {
+    }
+
+    /**
+     * Перезапустить задачу таймера.
+     *
+     * Restart timer task.
+     */
+    function restart()
+    {
+    }
+
+    /**
+     * Изменить время задержки и период таймера.
+     *
+     * Change the timer's due time and period.
+     *
+     * @param int $dueTime Время задержки в миллисекундах / Due time in milliseconds
+     * @param int $period Период в миллисекундах / Period in milliseconds
+     */
+    function change(int $dueTime, int $period)
+    {
+    }
+
+    /**
+     * Возвращает true, если таймер запущен, false если остановлен.
+     *
+     * Returns true if the timer is running, false if stopped.
+     *
+     * @var bool
+     */
+    public bool $isRunning;
+
+    /**
+     * Возвращает true, если таймер удален.
+     *
+     * Returns true if the timer is disposed.
+     *
+     * @var bool
+     */
+    public bool $isDisposed;
+
+    /**
+     * Возвращает запланированное время выполнения последней
+     * фактической выполнения этой задачи. (Если этот метод вызывается
+     * во время выполнения задачи, возвращаемое значение - это запланированное
+     * время выполнения текущей задачи).
+     *
      * Returns the scheduled execution time of the most recent
-     * actual execution of this task.  (If this method is invoked
+     * actual execution of this task. (If this method is invoked
      * while task execution is in progress, the return value is the scheduled
      * execution time of the ongoing task execution.)
+     *
+     * @return int
      */
-    function scheduledTime()
+    function scheduledTime(): int
     {
     }
 }
--- PeachPie Extension Libraries/Peachpie.Base.Ext/src/Threading/Timer.cs
+++ PeachPie Extension Libraries/Peachpie.Base.Ext/src/Threading/Timer.cs
@@ -1,101 +1,125 @@
+
 using System;
 using System.Collections.Concurrent;
 using System.Threading;
 using Pchp.Core;
+using Pchp.Library.Spl;
+using Exception = Pchp.Library.Spl.Exception;
 
 namespace Php.Threading
 {
     public class Timer : IDisposable
     {
-        private Context _ctx;
+        private readonly Context _ctx;
+        private readonly IPhpCallable _callback;
         private System.Threading.Timer _timer;
-        private DateTime _scheduledTime;
+        private int _dueTime;
+        private int _period;
+        private bool _isDisposed;
         private static readonly ConcurrentBag<Timer> _activeTimers = new ConcurrentBag<Timer>();
 
-        public Timer(Context ctx)
+        // Свойства для определения состояния таймера / Properties to determine the state of the timer
+        public bool isRunning => _timer != null && !_isDisposed;
+        public bool isDisposed => _isDisposed;
+
+        // Конструктор таймера / Timer constructor
+        private Timer(Context ctx, IPhpCallable callback, int dueTime, int period)
         {
             _ctx = ctx;
+            _callback = callback;
+            _dueTime = dueTime;
+            _period = period;
+            _isDisposed = false;
+            _timer = new System.Threading.Timer(TimerCallback, null, Timeout.Infinite, Timeout.Infinite);
             _activeTimers.Add(this);
         }
 
+        // Обратный вызов таймера / Timer callback
         private void TimerCallback(object state)
         {
-            IPhpCallable callback = (IPhpCallable)state;
             try
             {
-                callback.Invoke(_ctx);
+                _callback.Invoke(_ctx);
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"Timer callback error: {ex.Message}");
+                // Используем исключение из пространства имен Pchp.Library.Spl / Use exception from Pchp.Library.Spl namespace
+                throw new RuntimeException($"Timer callback error: {ex.Message}");
             }
         }
 
+        // Метод для изменения времени таймера / Method to change the timer's due time and period
+        public void Change(int dueTime, int period)
+        {
+            if (_isDisposed)
+            {
+                throw new InvalidOperationException("Cannot change a disposed timer.");
+            }
+
+            _dueTime = dueTime;
+            _period = period;
+            _timer.Change(dueTime, period);
+        }
+
+        // Метод для отмены таймера / Method to cancel the timer
         public void Cancel()
         {
             _timer?.Dispose();
             _timer = null;
-
-            if (_activeTimers.TryTake(out var removedTimer))
-            {
-                // Таймер успешно удален из активных таймеров
-            }
+            _isDisposed = true;
+            _activeTimers.TryTake(out _);
         }
 
+        // Метод для немедленного запуска таймера / Method to immediately run the timer
         public void Run()
         {
             _timer?.Change(0, Timeout.Infinite);
         }
 
-        public long ScheduledTime()
+        // Метод для остановки таймера / Method to stop the timer
+        public void Stop()
         {
-            return (long)(_scheduledTime - DateTime.UtcNow).TotalMilliseconds;
+            _timer?.Change(Timeout.Infinite, Timeout.Infinite);
         }
 
+        // Метод для перезапуска таймера / Method to restart the timer
+        public void Restart()
+        {
+            _timer?.Change(_dueTime, _period);
+        }
+
+        // Метод для создания таймера с задержкой / Method to create a timer with delay
         public static Timer SetTimeout(Context ctx, IPhpCallable callback, int milliseconds)
         {
-            var timer = new Timer(ctx);
-            timer._timer = new System.Threading.Timer(_ =>
-            {
-                try
-                {
-                    callback.Invoke(ctx);
-                }
-                finally
-                {
-                    timer.Cancel();
-                }
-            }, callback, milliseconds, Timeout.Infinite);
-            timer._scheduledTime = DateTime.UtcNow.AddMilliseconds(milliseconds);
+            var timer = new Timer(ctx, callback, milliseconds, Timeout.Infinite);
+            timer._timer.Change(milliseconds, Timeout.Infinite);
             return timer;
         }
 
+        // Метод для создания интервала таймера / Method to create an interval timer
         public static Timer SetInterval(Context ctx, IPhpCallable callback, int milliseconds)
         {
-            var timer = new Timer(ctx);
-            timer._timer = new System.Threading.Timer(_ =>
-            {
-                callback.Invoke(ctx);
-            }, callback, milliseconds, milliseconds);
-            timer._scheduledTime = DateTime.UtcNow.AddMilliseconds(milliseconds);
+            var timer = new Timer(ctx, callback, milliseconds, milliseconds);
+            timer._timer.Change(milliseconds, milliseconds);
             return timer;
         }
 
+        // Метод для создания таймера после заданного периода / Method to create a timer after a given period
         public static Timer After(Context ctx, string period, IPhpCallable callback)
         {
-            int milliseconds = ParsePeriod(period);
-            return SetTimeout(ctx, callback, milliseconds);
+            return SetTimeout(ctx, callback, ParsePeriod(period));
         }
 
+        // Метод для создания таймера, выполняющегося через регулярные интервалы / Method to create a timer that runs at regular intervals
         public static Timer Every(Context ctx, string period, IPhpCallable callback)
         {
-            int milliseconds = ParsePeriod(period);
-            return SetInterval(ctx, callback, milliseconds);
+            return SetInterval(ctx, callback, ParsePeriod(period));
         }
 
+        // Метод для создания таймера с условием / Method to create a conditional timer
         public static Timer Trigger(Context ctx, IPhpCallable condition, IPhpCallable callback, int checkPeriod)
         {
-            var timer = new Timer(ctx);
+            var timer = new Timer(ctx, callback, 0, checkPeriod);
             timer._timer = new System.Threading.Timer(_ =>
             {
                 if (condition.Invoke(ctx).ToBoolean())
@@ -103,56 +127,47 @@ namespace Php.Threading
                     callback.Invoke(ctx);
                     timer.Cancel();
                 }
-            }, condition, 0, checkPeriod);
-            timer._scheduledTime = DateTime.UtcNow.AddMilliseconds(checkPeriod);
+            }, null, 0, checkPeriod);
             return timer;
         }
 
+        // Метод для приостановки выполнения на заданный период / Method to sleep for a given period
         public static void Sleep(string period)
         {
-            int milliseconds = ParsePeriod(period);
-            Thread.Sleep(milliseconds);
+            Thread.Sleep(ParsePeriod(period));
         }
 
+        // Метод для разбора строкового периода в миллисекунды / Method to parse a period string into milliseconds
         public static int ParsePeriod(string period)
         {
             if (string.IsNullOrWhiteSpace(period))
-            {
                 throw new ArgumentException("Period is empty or null.");
-            }
 
             int milliseconds = 0;
-            string[] parts = period.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
-
-            foreach (string part in parts)
+            foreach (var part in period.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries))
             {
                 int length = part.Length;
                 char lastChar = part[length - 1];
-                int factor = 1;
-
-                switch (lastChar)
+                int factor = lastChar switch
                 {
-                    case 's': factor = 1000; break;
-                    case 'm': factor = 1000 * 60; break;
-                    case 'h': factor = 1000 * 60 * 60; break;
-                    case 'd': factor = 1000 * 60 * 60 * 24; break;
-                    default:
-                        if (!char.IsDigit(lastChar))
-                            throw new ArgumentException($"Invalid time unit '{lastChar}' in period.");
-                        break;
-                }
+                    's' => 1000,
+                    'm' => 1000 * 60,
+                    'h' => 1000 * 60 * 60,
+                    'd' => 1000 * 60 * 60 * 24,
+                    _ => char.IsDigit(lastChar) ? 1 : throw new ArgumentException($"Invalid time unit '{lastChar}' in period.")
+                };
 
                 string number = char.IsDigit(lastChar) ? part : part.Substring(0, length - 1);
                 if (!double.TryParse(number, out double num))
-                {
                     throw new ArgumentException($"Invalid number '{number}' in period.");
-                }
+
                 milliseconds += (int)(num * factor);
             }
 
             return milliseconds;
         }
 
+        // Метод для отмены всех активных таймеров / Method to cancel all active timers
         public static void CancelAll()
         {
             foreach (var timer in _activeTimers)
@@ -161,6 +176,7 @@ namespace Php.Threading
             }
         }
 
+        // Метод для освобождения ресурсов / Method to release resources
         public void Dispose()
         {
             Cancel();
